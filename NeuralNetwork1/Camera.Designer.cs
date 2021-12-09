6ec9ced923226eeeb42a3f149f4fa6b86d7ee50a
Andrew Vaimon <niko142000@mail.ru>
2021-12-09 17:01:57 +0300

    merge with camera

---
+++ NeuralNetwork1/Camera.Designer.cs
@@ -0,0 +1,295 @@
+﻿
+namespace NeuralNetwork1
+{
+    partial class Camera
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmbVideoSource = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.StartButton = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.originalImageBox = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.processedImgBox = new System.Windows.Forms.PictureBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tresholdTrackBar = new System.Windows.Forms.TrackBar();
+            this.label3 = new System.Windows.Forms.Label();
+            this.borderTrackBar = new System.Windows.Forms.TrackBar();
+            this.statusLabel = new System.Windows.Forms.Label();
+            this.ticksLabel = new System.Windows.Forms.Label();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.resolutionsBox = new System.Windows.Forms.ComboBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.originalImageBox)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.processedImgBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tresholdTrackBar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.borderTrackBar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cmbVideoSource
+            // 
+            this.cmbVideoSource.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.cmbVideoSource.FormattingEnabled = true;
+            this.cmbVideoSource.Location = new System.Drawing.Point(17, 612);
+            this.cmbVideoSource.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.cmbVideoSource.Name = "cmbVideoSource";
+            this.cmbVideoSource.Size = new System.Drawing.Size(326, 28);
+            this.cmbVideoSource.TabIndex = 1;
+            this.cmbVideoSource.SelectionChangeCommitted += new System.EventHandler(this.cmbVideoSource_SelectionChangeCommitted);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 588);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(119, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Выбор камеры";
+            // 
+            // StartButton
+            // 
+            this.StartButton.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.StartButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.StartButton.Location = new System.Drawing.Point(394, 635);
+            this.StartButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(188, 46);
+            this.StartButton.TabIndex = 3;
+            this.StartButton.Text = "Старт";
+            this.StartButton.UseVisualStyleBackColor = true;
+            this.StartButton.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.ticksLabel);
+            this.groupBox1.Controls.Add(this.resolutionsBox);
+            this.groupBox1.Controls.Add(this.originalImageBox);
+            this.groupBox1.Controls.Add(this.cmbVideoSource);
+            this.groupBox1.Controls.Add(this.StartButton);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(13, 14);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox1.Size = new System.Drawing.Size(756, 700);
+            this.groupBox1.TabIndex = 11;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Камера";
+            // 
+            // originalImageBox
+            // 
+            this.originalImageBox.Dock = System.Windows.Forms.DockStyle.Top;
+            this.originalImageBox.Location = new System.Drawing.Point(4, 24);
+            this.originalImageBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.originalImageBox.Name = "originalImageBox";
+            this.originalImageBox.Size = new System.Drawing.Size(748, 530);
+            this.originalImageBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.originalImageBox.TabIndex = 1;
+            this.originalImageBox.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.Control;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.checkBox1);
+            this.panel1.Controls.Add(this.btnSave);
+            this.panel1.Controls.Add(this.processedImgBox);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.statusLabel);
+            this.panel1.Controls.Add(this.borderTrackBar);
+            this.panel1.Controls.Add(this.tresholdTrackBar);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Location = new System.Drawing.Point(778, 18);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(666, 696);
+            this.panel1.TabIndex = 12;
+            // 
+            // processedImgBox
+            // 
+            this.processedImgBox.Location = new System.Drawing.Point(195, 5);
+            this.processedImgBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.processedImgBox.Name = "processedImgBox";
+            this.processedImgBox.Size = new System.Drawing.Size(300, 300);
+            this.processedImgBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.processedImgBox.TabIndex = 0;
+            this.processedImgBox.TabStop = false;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(268, 381);
+            this.checkBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(128, 24);
+            this.checkBox1.TabIndex = 24;
+            this.checkBox1.Text = "Обработать";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(499, 340);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 20);
+            this.label2.TabIndex = 23;
+            this.label2.Text = "Порог";
+            // 
+            // tresholdTrackBar
+            // 
+            this.tresholdTrackBar.LargeChange = 1;
+            this.tresholdTrackBar.Location = new System.Drawing.Point(426, 381);
+            this.tresholdTrackBar.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.tresholdTrackBar.Maximum = 255;
+            this.tresholdTrackBar.Name = "tresholdTrackBar";
+            this.tresholdTrackBar.Size = new System.Drawing.Size(210, 69);
+            this.tresholdTrackBar.TabIndex = 22;
+            this.tresholdTrackBar.TickFrequency = 25;
+            this.tresholdTrackBar.Value = 40;
+            this.tresholdTrackBar.ValueChanged += new System.EventHandler(this.tresholdTrackBar_ValueChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(102, 347);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 20);
+            this.label3.TabIndex = 20;
+            this.label3.Text = "Поля";
+            // 
+            // borderTrackBar
+            // 
+            this.borderTrackBar.LargeChange = 60;
+            this.borderTrackBar.Location = new System.Drawing.Point(20, 381);
+            this.borderTrackBar.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.borderTrackBar.Maximum = 160;
+            this.borderTrackBar.Name = "borderTrackBar";
+            this.borderTrackBar.Size = new System.Drawing.Size(210, 69);
+            this.borderTrackBar.TabIndex = 18;
+            this.borderTrackBar.TickFrequency = 10;
+            this.borderTrackBar.Value = 40;
+            this.borderTrackBar.ValueChanged += new System.EventHandler(this.borderTrackBar_ValueChanged);
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.AutoSize = true;
+            this.statusLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.statusLabel.Location = new System.Drawing.Point(38, 471);
+            this.statusLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(113, 32);
+            this.statusLabel.TabIndex = 24;
+            this.statusLabel.Text = "Статус:";
+            // 
+            // ticksLabel
+            // 
+            this.ticksLabel.AutoSize = true;
+            this.ticksLabel.Location = new System.Drawing.Point(390, 610);
+            this.ticksLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.ticksLabel.Name = "ticksLabel";
+            this.ticksLabel.Size = new System.Drawing.Size(194, 20);
+            this.ticksLabel.TabIndex = 30;
+            this.ticksLabel.Text = "Ticks for frame processing";
+            // 
+            // btnSave
+            // 
+            this.btnSave.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnSave.Location = new System.Drawing.Point(472, 641);
+            this.btnSave.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(188, 46);
+            this.btnSave.TabIndex = 25;
+            this.btnSave.Text = "Сохранить кадр";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // resolutionsBox
+            // 
+            this.resolutionsBox.AllowDrop = true;
+            this.resolutionsBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.resolutionsBox.FormattingEnabled = true;
+            this.resolutionsBox.Location = new System.Drawing.Point(18, 653);
+            this.resolutionsBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.resolutionsBox.Name = "resolutionsBox";
+            this.resolutionsBox.Size = new System.Drawing.Size(325, 28);
+            this.resolutionsBox.TabIndex = 34;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1460, 726);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.KeyPreview = true;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "MainForm";
+            this.Text = "Распознавалка";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.MainForm_KeyDown);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.originalImageBox)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.processedImgBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tresholdTrackBar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.borderTrackBar)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ComboBox cmbVideoSource;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button StartButton;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.PictureBox originalImageBox;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TrackBar borderTrackBar;
+        private System.Windows.Forms.Label statusLabel;
+        private System.Windows.Forms.Label ticksLabel;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TrackBar tresholdTrackBar;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.PictureBox processedImgBox;
+        private System.Windows.Forms.ComboBox resolutionsBox;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
---
+++ NeuralNetwork1/Processor.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace NeuralNetwork1
+{
+    internal class Settings
+    {
+        private int _border = 20;
+        public int border
+        {
+            get
+            {
+                return _border;
+            }
+            set
+            {
+                if ((value > 0) && (value < height / 3))
+                {
+                    _border = value;
+                    if (top > 2 * _border) top = 2 * _border;
+                    if (left > 2 * _border) left = 2 * _border;
+                }
+            }
+        }
+
+        public int width = 640;
+        public int height = 640;
+
+        /// <summary>
+        /// Размер сетки для сенсоров по горизонтали
+        /// </summary>
+        public int blocksCount = 10;
+
+        /// <summary>
+        /// Желаемый размер изображения до обработки
+        /// </summary>
+        public Size orignalDesiredSize = new Size(500, 500);
+        /// <summary>
+        /// Желаемый размер изображения после обработки
+        /// </summary>
+        public Size processedDesiredSize = new Size(300, 300);
+
+        public int margin = 10;
+        public int top = 40;
+        public int left = 40;
+
+        /// <summary>
+        /// Второй этап обработки
+        /// </summary>
+        public bool processImg = false;
+
+        /// <summary>
+        /// Порог при отсечении по цвету 
+        /// </summary>
+        public byte threshold = 120;
+        public float differenceLim = 0.15f;
+
+        public void incTop() { if (top < 2 * _border) ++top; }
+        public void decTop() { if (top > 0) --top; }
+        public void incLeft() { if (left < 2 * _border) ++left; }
+        public void decLeft() { if (left > 0) --left; }
+    }
+
+    internal class MagicEye
+    {
+        /// <summary>
+        /// Обработанное изображение
+        /// </summary>
+        public Bitmap processed;
+        /// <summary>
+        /// Оригинальное изображение после обработки
+        /// </summary>
+        public Bitmap original;
+
+        /// <summary>
+        /// Класс настроек
+        /// </summary>
+        public Settings settings = new Settings();
+
+
+
+        public MagicEye()
+        {
+        }
+
+        public bool ProcessImage(Bitmap bitmap)
+        {
+            // На вход поступает необработанное изображение с веб-камеры
+
+            //  Минимальная сторона изображения (обычно это высота)
+            if (bitmap.Height > bitmap.Width)
+                throw new Exception("К такой забавной камере меня жизнь не готовила!");
+            //  Можно было, конечено, и не кидаться эксепшенами в истерике, но идите и купите себе нормальную камеру!
+            int side = bitmap.Height;
+
+
+            //  Мы сейчас занимаемся тем, что красиво оформляем входной кадр, чтобы вывести его на форму
+            //Rectangle cropRect = new Rectangle((bitmap.Width - bitmap.Height) / 2 + settings.left + settings.border, settings.top + settings.border, side, side);
+            Rectangle cropRect = new Rectangle(settings.border, settings.border, bitmap.Width - settings.border, bitmap.Height - settings.border);
+            //  Тут создаём новый битмапчик, который будет исходным изображением
+
+            original = bitmap;
+
+            //  Объект для рисования создаём
+            //Graphics g = Graphics.FromImage(original);
+
+            //g.DrawImage(bitmap, new Rectangle(0, 0, original.Width, original.Height), cropRect, GraphicsUnit.Pixel);
+
+            //  Теперь всю эту муть пилим в обработанное изображение
+            AForge.Imaging.Filters.Crop cropFilter = new AForge.Imaging.Filters.Crop(cropRect);
+            var uProcessed = cropFilter.Apply(AForge.Imaging.UnmanagedImage.FromManagedImage(original));
+            AForge.Imaging.Filters.Grayscale grayFilter = new AForge.Imaging.Filters.Grayscale(0.2125, 0.7154, 0.0721);
+            uProcessed = grayFilter.Apply(uProcessed);
+
+
+
+            //  Масштабируем изображение до 500x500 - этого достаточно
+            AForge.Imaging.Filters.ResizeBilinear scaleFilter = new AForge.Imaging.Filters.ResizeBilinear(settings.processedDesiredSize.Width, settings.processedDesiredSize.Height);
+            uProcessed = scaleFilter.Apply(uProcessed);
+            //original = scaleFilter.Apply(original);
+
+            //  Пороговый фильтр применяем. Величина порога берётся из настроек, и меняется на форме
+            AForge.Imaging.Filters.BradleyLocalThresholding threshldFilter = new AForge.Imaging.Filters.BradleyLocalThresholding();
+            threshldFilter.PixelBrightnessDifferenceLimit = settings.differenceLim;
+            threshldFilter.ApplyInPlace(uProcessed);
+
+
+            if (settings.processImg)
+            {
+
+                string info = processSample(ref uProcessed);
+                Font f = new Font(FontFamily.GenericSansSerif, 20);
+
+            }
+
+            processed = uProcessed.ToManagedImage();
+
+            return true;
+        }
+
+        /// <summary>
+        /// Обработка одного сэмпла
+        /// </summary>
+        /// <param name="index"></param>
+        private string processSample(ref AForge.Imaging.UnmanagedImage unmanaged)
+        {
+            string rez = "Обработка";
+
+            ///  Инвертируем изображение
+            AForge.Imaging.Filters.Invert InvertFilter = new AForge.Imaging.Filters.Invert();
+            InvertFilter.ApplyInPlace(unmanaged);
+
+            ///    Создаём BlobCounter, выдёргиваем самый большой кусок, масштабируем, пересечение и сохраняем
+            ///    изображение в эксклюзивном использовании
+            AForge.Imaging.BlobCounterBase bc = new AForge.Imaging.BlobCounter();
+
+            bc.FilterBlobs = true;
+            bc.MinWidth = 3;
+            bc.MinHeight = 3;
+            // Упорядочиваем по размеру
+            bc.ObjectsOrder = AForge.Imaging.ObjectsOrder.Size;
+            // Обрабатываем картинку
+
+            bc.ProcessImage(unmanaged);
+
+            Rectangle[] rects = bc.GetObjectsRectangles();
+            rez = "Насчитали " + rects.Length.ToString() + " прямоугольников!";
+            //if (rects.Length == 0)
+            //{
+            //    finalPics[r, c] = AForge.Imaging.UnmanagedImage.FromManagedImage(new Bitmap(100, 100));
+            //    return 0;
+            //}
+
+            // К сожалению, код с использованием подсчёта blob'ов не работает, поэтому просто высчитываем максимальное покрытие
+            // для всех блобов - для нескольких цифр, к примеру, 16, можем получить две области - отдельно для 1, и отдельно для 6.
+            // Строим оболочку, включающую все блоки. Решение плохое, требуется доработка
+            int lx = unmanaged.Width;
+            int ly = unmanaged.Height;
+            int rx = 0;
+            int ry = 0;
+            for (int i = 0; i < rects.Length; ++i)
+            {
+                if (lx > rects[i].X) lx = rects[i].X;
+                if (ly > rects[i].Y) ly = rects[i].Y;
+                if (rx < rects[i].X + rects[i].Width) rx = rects[i].X + rects[i].Width;
+                if (ry < rects[i].Y + rects[i].Height) ry = rects[i].Y + rects[i].Height;
+            }
+
+            // Обрезаем края, оставляя только центральные блобчики
+            AForge.Imaging.Filters.Crop cropFilter = new AForge.Imaging.Filters.Crop(new Rectangle(lx, ly, rx - lx, ry - ly));
+            unmanaged = cropFilter.Apply(unmanaged);
+
+            //  Масштабируем до 100x100
+            AForge.Imaging.Filters.ResizeBilinear scaleFilter = new AForge.Imaging.Filters.ResizeBilinear(100, 100);
+            unmanaged = scaleFilter.Apply(unmanaged);
+
+            return rez;
+        }
+
+    }
+}
